bea196015c7829b76b410466257d7cc768f1d90a
Jia <zhangjia@mit.edu>
2014-10-12 14:21:46 -0400

    working on collapsing only children

--- test_noncollapse.js
+++ test_noncollapse.js
@@ -22,16 +22,20 @@ $(function() {
 function dataDidLoad(error, data1, data2) {
 	global.data1 = data1
 	//global.data2 = data2
-	dictionary1 = {}
-	formatMenuIntoSentences(global.data1.toLowerCase(), global.searchTerm, dictionary1)
-	console.log(convertTree(dictionary1))
-	drawChart(convertTree(dictionary1)[0],svg)
+	rightDictionary = {}
+	leftDictionary = {}
+	formatMenuIntoSentences(global.data1.toLowerCase(), global.searchTerm)
+	console.log(convertTree(rightDictionary))
+	console.log(convertTree(leftDictionary))
+	
+	drawChart(convertTree(rightDictionary)[0])
+	drawLeftChart(convertTree(leftDictionary)[0])
+	
 	//filterTree(convertTree(dictionary1)[0])
 //	formatMenuIntoSentences(global.data2.toLowerCase(), global.searchTerm, dictionary2)
 //	drawChart(convertTree(dictionary2)[0],treesvg2)
 
 	frm1.searchTerm.value = ""
-	dictionary1 = {}
 }
 
 //TODO: expand all
@@ -49,14 +53,19 @@ function searchFor() {
 	d3.select("#output").html("")
 	//console.log("searched for", enteredTerm)
 	if(frm1.searchTerm.value != ""){
-		formatMenuIntoSentences(global.data1.toLowerCase(), enteredTerm, dictionary1)
+		formatMenuIntoSentences(global.data1.toLowerCase(), enteredTerm)
 		//console.log(dictionary1)
-		drawChart(convertTree(dictionary1)[0],svg)
+		d3.select("#tree1 svg").remove()
+		drawChart(convertTree(rightDictionary)[0],svg)
+		drawLeftChart(convertTree(leftDictionary)[0], svg2)
+		
 		//displayResults()
 	}
 	//clear the input from form
 	frm1.searchTerm.value = ""
-	dictionary1 = {}
+	rightDictionary = {}
+	leftDictionary = {}
+	
 }
 
 document.onkeydown=function(){
@@ -67,14 +76,24 @@ document.onkeydown=function(){
 
 var restOfWords=null;
 
-function formatMenuIntoSentences(input, searchTerm, dictionary){
+function formatMenuIntoSentences(input, searchTerm){
 	//console.log(input)
 	var sentences = input.split(/\.\s*/g).map(function(sentence){return sentence.split(/\s+/g);});
 	//console.log(sentences)
 	for (var i =0; i < sentences.length; i++) {
 	    if(searchStringInArray(searchTerm, sentences[i]) > 0){
 			if(sentences[i].length > 3 ){
-		  	  	insertWords(dictionary, sentences[i], searchTerm);
+				var wordIndex = searchStringInArray(searchTerm, sentences[i])
+				var rightSentence = sentences[i].slice(wordIndex, sentences[i].length)
+				var leftSentence = sentences[i].slice(0, wordIndex+1)
+				//console.log(leftSentence)
+				
+				var leftSentence = leftSentence.reverse()
+				//console.log(leftSentence)
+		  	  	insertWords(rightDictionary, rightSentence, searchTerm);
+				if(leftSentence.length<30){
+				insertWords(leftDictionary, leftSentence, searchTerm)
+			}
 			}
 		}
 	}	
@@ -132,71 +151,63 @@ function convertTree(dictionary){
 
 
 /////DRAWING BELOW
+var width = 500
+var height =600
 
-var margin = {top: 20, right: 0, bottom: 20, left: 220},
-    width =1200 - margin.right - margin.left,
-    height = 600 - margin.top - margin.bottom;
 
-var i = 0,
-    duration = 250,
-    root;
-
-var tree = d3.layout.tree()
-    .size([height, width]);
-	var cluster = d3.layout.cluster()
-	    .size([height, width]);
-var diagonal = d3.svg.diagonal()
-    .projection(function(d) { return [d.y, d.x]; });
-
-var svg = d3.select("#tree1").append("svg")
-   .attr("width", width + margin.right + margin.left)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-	
-function drawChart(root, svg) {
-	d3.select("#tree1 svg").remove()
+	//attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+function drawChart(root) {
+	console.log(root.name.length)
+	var margin = root.name.length*23
 	var svg = d3.select("#tree1").append("svg")
-	   .attr("width", width + margin.right + margin.left)
-	    .attr("height", height + margin.top + margin.bottom)
-	  .append("g")
-	  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-	console.log("draw")
+		.attr("width", width+margin+350)
+		.attr("height", height)
+		.append("g")
+		.attr("transform", "translate(" + margin + "," + 0 + ")");
+	console.log(root)
+	var i = 0,
+	    duration = 250,
+	    root;
+	var tree = d3.layout.tree()
+	    .size([height, width]);
+		var cluster = d3.layout.cluster()
+		    .size([height, width+margin+350]);
+	var diagonal = d3.svg.diagonal()
+	    .projection(function(d) { return [d.y, d.x]; });
+	
 	var nodes = tree.nodes(root);
     var links = tree.links(nodes);
-	//  console.log(nodes)
-	var max = root.sibling
-    var textSizeScale = d3.scale.sqrt().domain([0,max]).range([1,50])
-	console.log(root)
-	 console.log(textSizeScale(max), max, root.sibling) 
-  var link = svg.selectAll(".link")
-      .data(links)
-    .enter().append("path")
-      .attr("class", "link")
-      .attr("d", diagonal);
-
-  var node = svg.selectAll(".node")
-      .data(nodes)
-    .enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
-
+	var maxSibling = root.sibling
+	var maxCount = root.count
+    var textSizeScale = d3.scale.sqrt().domain([0,maxSibling]).range([1,50])
+    var opacityScale = d3.scale.sqrt().domain([40, maxCount]).range([0.6,1]) 
+	var link = svg.selectAll(".link")
+		.data(links)
+  	 	.enter().append("path")
+		.attr("class", "link")
+		.attr("d", diagonal);
+  	var node = svg.selectAll(".node")
+      	.data(nodes)
+	    .enter().append("g")
+     	.attr("class", "node")
+     	.attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
   node.append("circle")
-      .attr("r", 0);
-
+      	.attr("r", 0);
   node.append("text")
       //.attr("dx", function(d) { return d.children ? -8 : 8; })
 	  .attr("dx", function(d) {//console.log(d.name, d.name.length, d.count); 
-		  return 10*d.name.length;})
+		  return 12*d.name.length*d.depth;})
       .attr("dy", function(d){
 		  return textSizeScale(d.depth)/2
 		  return textSizeScale(d.depth)
 	  })
+	  .style("fill-opacity", function(d){
+		  return opacityScale(d.count)
+	  })
       .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
       .text(function(d) { return d.name; })
 	  .style("font-size", function(d){
-		 // return 12
-		 if(d.count <2){
+		 if(d.count <1){
 			 return 0
 		 }
 		  return textSizeScale(d.sibling)
@@ -205,11 +216,86 @@ function drawChart(root, svg) {
 		  //console.log(d.name);
 		  originalSize = d3.select(this).style("font-size")
 		  //console.log(originalSize)
-		  d3.select(this).style("font-size", 30)
+		  d3.select(this).transition().style("font-size", 30)
 	  })
 	  .on("mouseout", function(d){
 		 // console.log(d.name); 
-		  d3.select(this).style("font-size", originalSize)
+		  d3.select(this).transition().style("font-size", originalSize)
 	  })
 }
+
+function drawLeftChart(root, side) {
+	var svg2 = d3.select("#tree2").append("svg")
+		.attr("width", width)
+		.attr("height", height)
+		.append("g")
+		.attr("transform", "translate(" + width + "," + 0 + ")");
+	var i = 0,
+	    duration = 250,
+	    root;
+	var tree = d3.layout.tree()
+	    .size([height, width]);
+		var cluster = d3.layout.cluster()
+		    .size([height, width]);
+			if(side == "right"){
+				var diagonal = d3.svg.diagonal()
+				    .projection(function(d) { return [d.y, d.x]; });
+			}else{
+				var diagonal = d3.svg.diagonal()
+				    .projection(function(d) { return [-d.y, d.x]; });
+			}
+	var nodes = tree.nodes(root);
+    var links = tree.links(nodes);
+	var maxSibling = root.sibling
+	var maxCount = root.count
+    var textSizeScale = d3.scale.sqrt().domain([0,maxSibling]).range([1,50])
+    var opacityScale = d3.scale.sqrt().domain([40, maxCount]).range([0.6,1]) 
+	var link = svg2.selectAll(".link")
+		.data(links)
+  	 	.enter().append("path")
+		.attr("class", "link")
+		.attr("d", diagonal);
+  	var node = svg2.selectAll(".node")
+      	.data(nodes)
+	    .enter().append("g")
+     	.attr("class", "node")
+     	.attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+  node.append("circle")
+      	.attr("r", 0);
+  node.append("text")
+      //.attr("dx", function(d) { return d.children ? -8 : 8; })
+	  .attr("dx", function(d) {//console.log(d.name, d.name.length, d.count); 
+		  return -12*d.name.length*d.depth;})
+      .attr("dy", function(d){
+		  return textSizeScale(d.depth)/2
+		  return textSizeScale(d.depth)
+	  })
+	  .style("fill-opacity", function(d){
+		  if(d.name == root.name){
+			  return 0
+		  }
+		  return opacityScale(d.count)
+	  })
+      .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
+      .text(function(d) { return d.name; })
+	  .style("font-size", function(d){
+		 if(d.count <1){
+			 return 0
+		 }
+		  return textSizeScale(d.sibling)
+	  })
+	  .on("mouseover", function(d){
+		  //console.log(d.name);
+		  originalSize = d3.select(this).style("font-size")
+		  //console.log(originalSize)
+		  d3.select(this).transition().style("font-size", 30)
+	  })
+	  .on("mouseout", function(d){
+		 // console.log(d.name); 
+		  d3.select(this).transition().style("font-size", originalSize)
+	  })
+}	
+
+//console.log(JSON.stringify(topLevelDictionary, null, 2));
+
 //console.log(JSON.stringify(topLevelDictionary, null, 2));
